b9ac2907459e1c3950c5f0589834186e83e53263
Fede Biaggini <fede_biaggini@hotmail.com>
2022-12-04 20:12:46 -0300

    tarea_clase_11

---
+++ js/ejerc11.js
@@ -0,0 +1,67 @@
+
+//let diaSemana = prompt("¿Qué día es hoy?");
+//switch (diaSemana) {
+//case "lunes":
+//alert("Hoy es lunes.");
+//break;
+//case "martes":
+//alert("Hoy es martes.");
+//break;
+//case "miércoles":
+//alert("Hoy es miércoles.");
+//break;
+//case "jueves":
+//alert("Hoy es jueves.");
+//break;
+//case "viernes":
+//alert("Hoy es viernes.");
+//break;
+//case "sábado":
+//alert("Hoy es sábado.");
+//break;
+//case "domingo":
+//alert("Hoy es domingo.");
+//break;
+//default:
+//alert("Escribe el día de la semana en minúsculas.");
+//}
+
+//let diaSemana = prompt("¿Qué día es hoy?");
+//switch (diaSemana) {
+//case "lunes":
+//alert("Hoy tocan flexiones.");
+//break;
+//case "martes":
+//alert("Hoy toca correr.");
+//break;
+//case "miércoles":
+//alert("Hoy toca abdominales.");
+//break;
+//case "jueves":
+//alert("Hoy toca sentadillas.");
+//break;
+//case "viernes":
+//alert("Hoy toca saltar.");
+//break;
+//case "sábado":
+//alert("Hoy toca caminar.");
+//break;
+//case "domingo":
+//alert("Hoy toca descanso. Y mañana con todo!");
+//break;
+//default:
+//alert("Escribe el día de la semana en minúsculas.");
+//}
+
+//for(i=0;i<=4;i++){
+//    document.write("El número es: " + i + "<br>");
+//    }
+
+//for(i=20;i<=70;i++){
+//document.write("El número es: " + i + "<br>");
+//}
+
+let repeticiones = parseInt(prompt("ingrese cantidad de iteraciones"));
+for (i = 0; i < repeticiones; i++) {
+    console.log("Hola Mundo");
+}
